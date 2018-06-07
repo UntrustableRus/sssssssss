a97efb1f0c5122789bb7edcc8957ba8c04d1959a
8ctopotamus <joshnaylor88@gmail.com>
2018-06-06 22:58:22 -0500

    Player as Class

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ### Working through another Multiplayer Game Web-Sockets tutorial
 
-Tutorial: https://www.youtube.com/watch?v=PfSwUOBL1YQ&t=399s
+Tutorial:
+* Starting point: https://www.youtube.com/watch?v=PfSwUOBL1YQ&t=399s
+* Part 5: https://www.youtube.com/watch?v=Tm-PXo9udWQ&t=49s
--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const serv = require('http').Server(app)
 const io = require('socket.io')(serv, {})
 
+// serve
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/client/index.html')
 })
@@ -12,46 +13,67 @@ serv.listen(2000, () => {
   console.log('App listening on port 2000!')
 })
 
+// game
 let SOCKET_LIST = {}
-let PLAYER_LIST = {}
 
-var Player = function(id) {
+var Entity = function(id) {
   var self = {
     x: 250,
     y: 250,
-    id: id,
-    number: "" + Math.floor(10 * Math.random()),
-    pressingRight: false,
-    pressingLeft: false,
-    pressingUp: false,
-    pressingDown: false,
-    maxSpd: 10,
+    spdX: 0,
+    spdY: 0,
+    id: ""
+  }
+  self.update = function() {
+    self.updatePosition()
   }
   self.updatePosition = function() {
-    if(self.pressingRight)
-      self.x += self.maxSpd
-    if(self.pressingLeft)
-      self.x -= self.maxSpd
-    if(self.pressingUp)
-      self.y -= self.maxSpd
-    if(self.pressingDown)
-      self.y += self.maxSpd
+    self.x += self.spdX
+    self.y += self.spdY
   }
   return self
 }
 
-io.on('connection', (socket) => {
-  socket.id = Math.random()
-  SOCKET_LIST[socket.id] = socket
+var Player = function(id) {
+  var self = Entity()
+  self.id = id
+  self.number = "" + Math.floor(10 * Math.random())
+  self.pressingRight = false
+  self.pressingLeft = false
+  self.pressingUp = false
+  self.pressingDown = false
+  self.maxSpd = 10
 
+  var super_update = self.update
+
+  self.update = function() {
+    self.updateSpd()
+    super_update()
+  }
+
+  self.updateSpd = function() {
+    if(self.pressingRight)
+      self.spdX = self.maxSpd
+    else if(self.pressingLeft)
+      self.spdX = -self.maxSpd
+    else
+      self.spdX = 0
+
+    if(self.pressingUp)
+      self.spdY = -self.maxSpd
+    else if(self.pressingDown)
+      self.spdY = self.maxSpd
+    else
+      self.spdY = 0
+  }
+  Player.list[id] = self
+  return self
+}
+
+Player.list = {}
+
+Player.onConnect = function(socket) {
   var player = Player(socket.id)
-  PLAYER_LIST[socket.id] = player
-
-  socket.on('disconnect', (socket) => {
-    delete SOCKET_LIST[socket.id]
-    delete PLAYER_LIST[socket.id]
-  })
-
   socket.on('keyPress', (data) => {
     if(data.inputId === 'left')
       player.pressingLeft = data.state
@@ -62,19 +84,41 @@ io.on('connection', (socket) => {
     else if(data.inputId === 'down')
       player.pressingDown = data.state
   })
-})
+}
 
-setInterval(function() {
+Player.onDisconnect = function(socket) {
+  delete Player.list[socket.id]
+}
+
+Player.update = function() {
   let pack = []
-  for (let i in PLAYER_LIST) {
-    const player = PLAYER_LIST[i]
-    player.updatePosition()
+  for (let i in Player.list) {
+    const player = Player.list[i]
+    player.update()
     pack.push({
       x: player.x,
       y: player.y,
       number: player.number
     })
   }
+  return pack
+}
+
+io.on('connection', (socket) => {
+  socket.id = Math.random()
+  SOCKET_LIST[socket.id] = socket
+
+  Player.onConnect(socket)
+
+  socket.on('disconnect', (socket) => {
+    delete SOCKET_LIST[socket.id]
+    Player.onDisconnect(socket)
+  })
+})
+
+// game loop
+setInterval(function() {
+  var pack = Player.update()
   for(let i in SOCKET_LIST) {
     const socket = SOCKET_LIST[i]
     socket.emit('newPosition', pack)
